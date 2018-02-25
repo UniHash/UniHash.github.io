4ed32b16ba840155ec9cf24ece315f6eff4576ae
UniHash <unihasher@gmail.com>
2018-02-25 11:10:17 -0800

    Added favicon and fixed text to make it look better

--- scripts/msloader.js
+++ scripts/msloader.js
@@ -23,41 +23,76 @@ if (!currency) {
     }
 }
 
+var currencyName = currency;
+var currencyPool = "Pool not set!";
 switch(currency) {
     case "ETN":
+        currencyName = "Electroneum";
+        currencyPool = "etn.spacepools.org";
         break;
 
     case "TRTL":
+        currencyName = "Turtle";
+        currencyPool = "z-pool.com";
         break;
 
     case "GRFT":
+        currencyName = "Graft";
+        currencyPool = "www.graftpool.online";
         break;
 
     case "SUMO":
+        currencyName = "Sumokoin";
+        currencyPool = "sumokoin.hashvault.pro";
         break;
 
     case "DERO":
+        currencyName = "Dero";
+        currencyPool = "pool.dero.live";
         break;
 
     case "ITNS":
+        currencyName = "Intense";
+        currencyPool = "intense.hashvault.pro";
         break;
 
     default:
         console.log("Unknown currency!");
         currency = "ETN";
+        currencyName = "Electroneum";
+        currencyPool = "etn.spacepools.org";
         break;
 }
 
 setTimeout(function(){
 var bdy = document.getElementById("body");
-bdy.innerHTML = `
-    <p>This is the place to mine! It is still a heavy work in progress. Click to toggle on and off. You may need to turn off your ad-block and put an exception in your antivirus to use this.
+bdy.innerHTML = `<center>
+    Welcome to the ` + currencyName + ` Mineshaft.<br><br>
     <br>
-    Also make sure you have your wallet address in the url (as the <a href="..">quarry<a> page shows) or it will not mine to your address!
-    <br> Here are some numbers you may like</p>
+    Current Pool Settings: <a href="//` + currencyPool + `" target="_blank">` + currencyPool + `</a><br>
+    <br>
+    <img src="../images/` + currency + `.png" style="width:100px;height:100px;">
+    <br>
+    <br>
+    You may need to turn off your ad-block and put an exception in your antivirus to use this.
+    <br>
+    <b>Make sure you have your wallet address in the url (as the <a href="..">quarry<a> page shows) or it will not mine to your address! </b>
+    <br><br><br>
+    This represents your current CPU Hashrate:
     <div style="color:blue;cursor:pointer;" id="hashdiv" onclick="toggleminer();">
         |<span id="hs">0</span> H/s|<span id="ah">0</span> Accepted Hashes|<span id="th">0</span> Total Hashes|
     </div>
+    <br>
+    Click the text above to toggle (on/off). 
+    <br>
+    <span style="color:red;">Red = OFF</span> <br>
+    <span style="color:blue;"> Blue = ON </span>
+    <br><br>
+    <br>
+    (You may not see stats on the pool until you get Accepted Hashes)
+    <br>
+    For any of your other ` + currencyName + ` needs, a good place to start is here: <a href="//coinmarketcap.com/currencies/` + currencyName + `" target="_blank">https://coinmarketcap.com/currencies/` + currencyName + `</a>. (` + currencyName + ` at coin market cap)
+    </center>
 `;
 var title = currency;
 if (typeof obscure !== 'undefined' && obscure) {
